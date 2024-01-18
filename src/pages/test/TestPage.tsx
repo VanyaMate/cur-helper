f503e0b567479df7e2ae549bc949d4ec2070d4fa
VanyaMate <buhu653@mail.ru>
2024-01-19 02:26:33 +0300

    restyle test-page

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -8,6 +8,7 @@ import OrderedList from '@/components/ui/list/OrderedList/OrderedList.tsx';
 import TestItemLink from '@/components/common/test/TestItemLink/TestItemLink.tsx';
 import { useNavigate } from 'react-router-dom';
 import { usePageUrl } from '@/hooks/page/usePageUrl.ts';
+import Collapse from '@/components/ui/collapse/Collapse/Collapse.tsx';
 
 
 export type TestPageProps = {}
@@ -39,90 +40,92 @@ const TestPage: React.FC<TestPageProps> = (props) => {
                 />
                 <Button styleType={ 'main' }>Найти</Button>
             </aside>
-            <OrderedList
-                prefix={ '1' }
-                title={ 'Общие правила' }
-                list={ [
-                    <TestItemLink
-                        id={ '1-1' }
-                        disabled={ false }
-                        label={ 'Законы' }
-                        onClick={ navigateCallback }
-                        status={ 'satisfactorily' }
-                        questions={ 17 }
-                        rightAnswers={ 14 }
-                    />,
-                    <TestItemLink
-                        id={ '1-2' }
-                        disabled={ false }
-                        label={ 'Правила' }
-                        onClick={ navigateCallback }
-                        status={ 'unsatisfactory' }
-                        questions={ 16 }
-                        rightAnswers={ 7 }
-                    />,
-                    <TestItemLink
-                        id={ '1-3' }
-                        disabled={ false }
-                        label={ 'Этикет' }
-                        onClick={ navigateCallback }
-                        status={ 'perfect' }
-                        questions={ 21 }
-                        rightAnswers={ 21 }
-                    />,
-                ] }
-            />
-            <OrderedList
-                prefix={ '2' }
-                title={ 'Правила общения' }
-                list={ [
-                    <TestItemLink
-                        id={ '2-1' }
-                        disabled={ false }
-                        label={ 'Вежливость' }
-                        onClick={ navigateCallback }
-                        status={ 'satisfactorily' }
-                        questions={ 11 }
-                        rightAnswers={ 7 }
-                    />,
-                    <TestItemLink
-                        id={ '2-2' }
-                        disabled={ false }
-                        label={ 'Открытость' }
-                        onClick={ navigateCallback }
-                        status={ 'perfect' }
-                        questions={ 13 }
-                        rightAnswers={ 13 }
-                    />,
-                    <TestItemLink
-                        id={ '2-3' }
-                        disabled={ false }
-                        label={ 'Конкретика' }
-                        onClick={ navigateCallback }
-                        status={ 'not-started' }
-                        questions={ 27 }
-                        rightAnswers={ 0 }
-                    />,
-                    <TestItemLink
-                        id={ '2-4' }
-                        disabled={ true }
-                        label={ 'Обвинения' }
-                        onClick={ navigateCallback }
-                        status={ 'not-started' }
-                        questions={ 13 }
-                        rightAnswers={ 0 }
-                    />,
-                    <TestItemLink
-                        id={ '2-5' }
-                        disabled={ true }
-                        label={ 'Вопросы' }
-                        onClick={ navigateCallback }
-                        status={ 'not-started' }
-                        questions={ 14 }
-                        rightAnswers={ 0 }
-                    />,
-                ] }
-            />
+            <Collapse title={ '1. Общие правила' } opened={ true } item={ 'main' }>
+                <OrderedList
+                    prefix={ '1' }
+                    list={ [
+                        <TestItemLink
+                            id={ '1-1' }
+                            disabled={ false }
+                            label={ 'Законы' }
+                            onClick={ navigateCallback }
+                            status={ 'satisfactorily' }
+                            questions={ 17 }
+                            rightAnswers={ 14 }
+                        />,
+                        <TestItemLink
+                            id={ '1-2' }
+                            disabled={ false }
+                            label={ 'Правила' }
+                            onClick={ navigateCallback }
+                            status={ 'unsatisfactory' }
+                            questions={ 16 }
+                            rightAnswers={ 7 }
+                        />,
+                        <TestItemLink
+                            id={ '1-3' }
+                            disabled={ false }
+                            label={ 'Этикет' }
+                            onClick={ navigateCallback }
+                            status={ 'perfect' }
+                            questions={ 21 }
+                            rightAnswers={ 21 }
+                        />,
+                    ] }
+                />
+            </Collapse>
+            <Collapse title={ '2. Правила общения' } opened={ true } item={ 'main' }>
+                <OrderedList
+                    prefix={ '2' }
+                    list={ [
+                        <TestItemLink
+                            id={ '2-1' }
+                            disabled={ false }
+                            label={ 'Вежливость' }
+                            onClick={ navigateCallback }
+                            status={ 'satisfactorily' }
+                            questions={ 11 }
+                            rightAnswers={ 7 }
+                        />,
+                        <TestItemLink
+                            id={ '2-2' }
+                            disabled={ false }
+                            label={ 'Открытость' }
+                            onClick={ navigateCallback }
+                            status={ 'perfect' }
+                            questions={ 13 }
+                            rightAnswers={ 13 }
+                        />,
+                        <TestItemLink
+                            id={ '2-3' }
+                            disabled={ false }
+                            label={ 'Конкретика' }
+                            onClick={ navigateCallback }
+                            status={ 'not-started' }
+                            questions={ 27 }
+                            rightAnswers={ 0 }
+                        />,
+                        <TestItemLink
+                            id={ '2-4' }
+                            disabled={ true }
+                            label={ 'Обвинения' }
+                            onClick={ navigateCallback }
+                            status={ 'not-started' }
+                            questions={ 13 }
+                            rightAnswers={ 0 }
+                        />,
+                        <TestItemLink
+                            id={ '2-5' }
+                            disabled={ true }
+                            label={ 'Вопросы' }
+                            onClick={ navigateCallback }
+                            status={ 'not-started' }
+                            questions={ 14 }
+                            rightAnswers={ 0 }
+                        />,
+                    ] }
+                />
+            </Collapse>
         </Section>
     );
 };
