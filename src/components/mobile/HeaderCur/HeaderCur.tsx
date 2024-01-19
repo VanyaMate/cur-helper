c8cea5142761ab46e41138e568908d6d15ced52f
VanyaMate <buhu653@mail.ru>
2024-01-19 03:02:48 +0300

    add temporaly toggle theme button

--- src/components/mobile/HeaderCur/HeaderCur.tsx
+++ src/components/mobile/HeaderCur/HeaderCur.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Section from '@/components/ui/container/Section/Section.tsx';
 import Link from '@/components/ui/link/Link/Link.tsx';
 import css from './HeaderCur.module.scss';
+import Button from '@/components/ui/button/Button/Button.tsx';
+import IconM from '@/components/ui/icon/IconM.tsx';
+import SpaceBetween from '@/components/ui/container/flex/SpaceBetween/SpaceBetween.tsx';
 
 
 export type HeaderCurProps = {
@@ -11,14 +14,30 @@ export type HeaderCurProps = {
 const HeaderCur: React.FC<HeaderCurProps> = (props) => {
     const { region } = props;
 
+    const toggleTheme = useCallback(() => {
+        const body: HTMLBodyElement = document.querySelector('body')!;
+        const isDark: boolean       = body.classList.contains('dark');
+        if (isDark) {
+            body.classList.remove('dark');
+            body.classList.add('dark-blue');
+        } else {
+            body.classList.add('dark');
+            body.classList.remove('dark-blue');
+        }
+    }, []);
+
     return (
-        <Section type={ 'section' } item={ 'main' } className={ css.container }>
-            <img
-                src={ '/cur-logo.png' }
-                className={ css.logo }
-            />
-            <Link to={ '#' }>{ region }</Link>
-        </Section>
+        <SpaceBetween type={ 'section' } item={ 'main' } className={ css.container }>
+            <SpaceBetween>
+                <img
+                    src={ '/cur-logo.png' }
+                    className={ css.logo }
+                />
+                <Link to={ '#' }>{ region }</Link>
+            </SpaceBetween>
+            <Button onClick={ toggleTheme }
+                    styleType={ 'main' }><IconM>refresh</IconM></Button>
+        </SpaceBetween>
     );
 };
 
