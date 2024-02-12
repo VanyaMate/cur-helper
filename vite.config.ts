b4fa107822350d822b036d7de54762f230ea48d8
VanyaMate <buhu653@mail.ru>
2024-02-12 10:58:53 +0300

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,56 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ react() ],
+    plugins: [
+        react(),
+        VitePWA({
+            injectRegister: 'auto',
+            registerType  : 'autoUpdate',
+            workbox       : {
+                cleanupOutdatedCaches: true,
+                cacheId              : 'v0.0.1',
+            },
+            manifest      : {
+                'name'            : 'ЦУР Помощник',
+                'short_name'      : 'ЦУР',
+                'theme_color'     : '#84A0EF',
+                'background_color': '#23252D',
+                'display'         : 'standalone',
+                'orientation'     : 'portrait',
+                'start_url'       : '.',
+                'icons'           : [
+                    {
+                        'src'  : '/cur-logo.png',
+                        'sizes': '64x64',
+                        'type' : 'image/png',
+                    },
+                    {
+                        'src'  : '/cur-logo.png',
+                        'sizes': '192x192',
+                        'type' : 'image/png',
+                    },
+                    {
+                        'src'  : '/cur-logo.png',
+                        'sizes': '256x256',
+                        'type' : 'image/png',
+                    },
+                    {
+                        'src'  : '/cur-logo.png',
+                        'sizes': '384x384',
+                        'type' : 'image/png',
+                    },
+                    {
+                        'src'  : '/cur-logo.png',
+                        'sizes': '512x512',
+                        'type' : 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
     css    : {
         modules: {
             generateScopedName: '[name]_[local]_[hash:base64:5]',
@@ -13,5 +60,8 @@ export default defineConfig({
         alias: {
             '@': '/src',
         },
+    },
+    build  : {
+        manifest: true,
     },
 });
