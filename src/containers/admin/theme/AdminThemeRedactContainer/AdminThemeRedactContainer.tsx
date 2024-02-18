60cac1e84ca9f5dabf7779ccb981c1cde87a4fff
VanyaMate <buhu653@mail.ru>
2024-02-18 18:28:42 +0300

    init

--- src/containers/admin/theme/AdminThemeRedactContainer/AdminThemeRedactContainer.tsx
+++ src/containers/admin/theme/AdminThemeRedactContainer/AdminThemeRedactContainer.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Section from '@/components/ui/container/Section/Section.tsx';
-import Input from '@/components/ui/input/Input/Input.tsx';
 import OrderedList from '@/components/ui/list/OrderedList/OrderedList.tsx';
-import P from '@/components/ui/p/P/P.tsx';
+import Title from '@/components/ui/title/Title/Title.tsx';
+import EditorJS from '@editorjs/editorjs';
+import { THEME_EDITOR_TOOLS } from '@/containers/editorjs/tools/theme-editor.tools.ts';
 
 
 export type AdminThemeRedactContainerProps = {};
 
 const AdminThemeRedactContainer: React.FC<AdminThemeRedactContainerProps> = (props) => {
-    const {} = props;
+    const {}     = props;
+    const editor = useRef<EditorJS>();
+
+    useEffect(() => {
+        if (!editor.current) {
+            editor.current = new EditorJS({
+                data  : JSON.parse(localStorage.getItem('editorjs') ?? '{}') ?? '',
+                tools : THEME_EDITOR_TOOLS,
+                holder: 'content',
+                onChange (): void {
+                    if (editor.current && !editor.current.readOnly) {
+                        editor.current?.save().then((data) => {
+                            localStorage.setItem('editorjs', JSON.stringify(data));
+                        });
+                    }
+                },
+            });
+        }
+    }, []);
 
     return (
         <Section>
@@ -31,25 +50,8 @@ const AdminThemeRedactContainer: React.FC<AdminThemeRedactContainerProps> = (pro
                 selfIndex={ [ '1', '', '3' ] }
             />
             <br/>
-            <br/>
-            <hr/>
-            <br/>
-            <br/>
-            <P type="invisible">Публичный ID</P>
-            <Input onChangeHandler={ () => console.log() } value="1-1"/>
-            <P type="invisible">Заголовок</P>
-            <Input onChangeHandler={ () => console.log() } value="Законы и законы"/>
-            <P type="invisible">Описание</P>
-            <Input onChangeHandler={ () => console.log() }
-                   value="Основы этики и профессионального поведения для государственных служащих. Включает примеры конфликта интересов и этическое руководство для повседневных ситуаций"/>
-            <P type="invisible">Дополнительная информация</P>
-            <Input onChangeHandler={ () => console.log() } value="На момент 2023 года"/>
-            <P type="invisible">Текст</P>
-            <Input onChangeHandler={ () => console.log() }
-                   value="Тема охватывает основные принципы этики в государственной основы этики и профессионального поведения для государственных"/>
-            <P>Editor.js / Lexical</P>
-            <P>Tiptap [headless]</P>
-            <P>Своё?</P>
+            <Title>EditorJS</Title>
+            <div id="content"/>
         </Section>
     );
 };
